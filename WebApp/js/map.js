d1e97bcb3629f897c96beb081c725a81a37245f9
Timothy Walker <twalker@bb136-21.adit.mines.edu>
2017-05-17 12:58:19 -0600

    Web app working

--- WebApp/js/map.js
+++ WebApp/js/map.js
@@ -3,8 +3,43 @@ function initMap() {
 	var center = {lat: 40.0, lng: -105.0};
 	var zoom = 10;
 
-	var map = new google.maps.Map(document.getElementById('map'), {
+	map = new google.maps.Map(document.getElementById('map'), {
 		zoom: zoom,
 		center: center
 	});
+}
+
+var markers = [];
+
+function submitQuery() {
+	var bounds = map.getBounds();
+	var ne = bounds.getNorthEast();
+	var sw = bounds.getSouthWest();
+	var east = ne.lng();
+	var west = sw.lng();
+	var north = ne.lat();
+	var south = sw.lat();
+
+	var transport = new Thrift.TXHRTransport("http://localhost:8000/service");
+	var protocol = new Thrift.TJSONProtocol(transport);
+	var client = new GeolocationServiceClient(protocol);
+
+	var result = client.getFeatures(west, east, south, north);
+
+	for (var i = 0; i < markers.length; i++) {
+		markers[i].setMap(null);
+	}
+	markers = [];
+//	console.log(result);
+	for (var i = 0; i < result.length; i++) {
+		var latlng = {lat: parseFloat(result[i].longitude), 
+			lng: parseFloat(result[i].latitude)};
+		var marker = new google.maps.Marker({
+			position: latlng,
+			map: map,
+			title: result[i].id.toString()
+		});
+		markers.push(marker);
+	}
+
 }
