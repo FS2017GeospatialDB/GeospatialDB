4a6a8b6e22c073dd2f717c795b23847a2adc37df
Ryan Hunt <ryhunt@mymail.mines.edu>
2017-05-16 23:55:01 -0600

    Started webapp, including GMap and Panel.

---
+++ WebApp/js/map.js
@@ -0,0 +1,10 @@
+function initMap() {
+	/* Fix This! */
+	var center = {lat: 40.0, lng: -105.0};
+	var zoom = 10;
+
+	var map = new google.maps.Map(document.getElementById('map'), {
+		zoom: zoom,
+		center: center
+	});
+}
