70c7f3f7255d019941fb9b8c38e4973d8aaeaaf4
Khanh Duong <kduong@mymail.mines.edu>
2017-05-31 15:12:01 -0600

    Points performing as expected, currently working on polygons

--- simp_and_store/load.py
+++ simp_and_store/load.py
@@ -1,7 +1,7 @@
 import sys
 import ctypes
 import geojson
-import s2sphere
+from s2 import *
 import uuid
 import time
 from sets import Set
@@ -32,9 +32,9 @@ def load():
 		# A list of every lon,lat pairs a feature has
 		#ptlist = []
 
-		if (featuretype == "Point"): case1(featurecoord, session, osm_id, json, ins_statement)
+		#if (featuretype == "Point"): case1(featurecoord, session, osm_id, json, ins_statement)
 		#if (featuretype == "LineString" or featuretype == "MultiPoint"): case2(featurecoord)
-		#if (featuretype == "Polygon" or featuretype == "MultiLineString"): case3(featurecoord)
+		if (featuretype == "Polygon" or featuretype == "MultiLineString"): case3(featurecoord, session, osm_id, json, ins_statement)
 		#if (featuretype == "MultiPolygon"): case4(featurecoord)
 
 		# Set to track distinct S2 values
@@ -66,27 +66,26 @@ def bbox(ptlist, session, osm_id, json, ins_statement):
 		if (pairs[1] < minlat): minlat = pairs[1]
 		if (pairs[1] > maxlat): maxlat = pairs[1]
 
-	region_rect = s2sphere.LatLngRect(s2sphere.LatLng.from_degrees(minlat,minlon),s2sphere.LatLng.from_degrees(maxlat,maxlon))
+	region_rect = S2LatLngRect(S2LatLng.FromDegrees(minlat,minlon),S2LatLng.FromDegrees(maxlat,maxlon))
 
 	# Region Coverer max 1 cell to get parent
-	get_parent = s2sphere.RegionCoverer()
+	get_parent = S2RegionCoverer()
 	get_parent.max_cells = 1
-	parent_array = get_parent.get_covering(region_rect) # Has only 1 element, the parent
-	parent = s2sphere.Cell(parent_array[0])
+	parent_array = get_parent.GetCovering(region_rect) # Has only 1 element, the parent
+	parent_cell = S2Cell(parent_array[0])
+	#print(parent_cell.level())
 
 	# Region Coverer using parents level and parent-1, to split if needed
 	# If feature covers all 4 squares in parent-1, it makes sense to just store in parent
-	coverer = s2sphere.RegionCoverer()
+	coverer = S2RegionCoverer()
 	coverer.max_cells = 3
-	coverer.max_level = parent.level()+1
-	coverer.min_level = parent.level()
-	cover_array = coverer.get_covering(region_rect)
+	coverer.max_level = parent_cell.level()+1
+	coverer.min_level = parent_cell.level()
+	cover_array = coverer.GetCovering(region_rect)
 
 	for cellid in cover_array:
-		new_cell = s2sphere.Cell(cellid)
+		new_cell = S2Cell(cellid)
 		cell_value = ctypes.c_long(cellid.id()).value
-		#print(new_cell.id())
-		#print(cell_value)
 		session.execute(ins_statement, (new_cell.level(), cell_value, uuid.uuid1(), osm_id, json))
 
 
@@ -95,20 +94,22 @@ def case1(featurecoord, session, osm_id, json, ins_statement):
 	ptlist.append(featurecoord)
 	bbox(ptlist, session, osm_id, json, ins_statement)
 	
-
+# TODO
 def case2(featurecoord):
 	ptlist = []
 	for coords in featurecoord:
 		ptlist.append(coords)
 	return ptlist
 
-def case3(featurecoord):
+# TODO
+def case3(featurecoord, session, osm_id, json, ins_statement):
 	ptlist = []
 	for coords in featurecoord:
 		for cs in coords:
 			ptlist.append(cs)
-	return ptlist
-		
+	bbox(ptlist, session, osm_id, json, ins_statement)
+	
+# TODO	
 def case4(featurecoord):
 	ptlist = []
 	for coords in featurecoord:
