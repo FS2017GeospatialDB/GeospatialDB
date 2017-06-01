bd7f9eb5e6d2ef74b3638055f2765cd6cedb663a
Huan Wang <huhwang@mines.edu>
2017-06-01 14:03:08 -0600

    fix geohelper, getbbox should work as expected

--- simp_and_store/load_refactor/geohelper.py
+++ simp_and_store/load_refactor/geohelper.py
@@ -1,21 +1,24 @@
 '''Geo helper class'''
 
-def is_correct_lat_range(lat_ranges, pt_list, percent=.3):
-    '''Check if the coordinates fall in the lat_range. Percent to check can be defined as
+import math
+
+
+def is_correct_lng_range(lng_ranges, pt_list, percent=.3):
+    '''Check if the coordinates fall in the lng_range. Percent to check can be defined as
     the last argument
 
-    lat_range: list[tuple(min_lat, max_lat)]'''
+    lng_range: list[tuple(min_lng, max_lng)]'''
 
-    def in_range(lat, lat_range):
+    def in_range(lng, lng_range):
         '''Test if the lat is in the given range'''
-        return True if lat >= lat_range[0] and lat <= lat_range[1] else False
+        return True if lng >= lng_range[0] and lng <= lng_range[1] else False
 
     num_to_check = int(math.ceil(percent * len(pt_list)))
     i = 0
     for pt in pt_list:
-        lat = pt[1]
-        for lat_range in lat_ranges:
-            if not in_range(lat, lat_range):
+        lng = pt[0]
+        for lng_range in lng_ranges:
+            if not in_range(lng, lng_range):
                 return False
         i = i + 1
         if i == num_to_check:
@@ -23,50 +26,51 @@ def is_correct_lat_range(lat_ranges, pt_list, percent=.3):
     return True
 
 
-def get_bboxes(pt_list, return_lat_first):
+def find_xtrem_coord(pt_list):
+    '''Find the min&max lat&lng for the feature'''
+    min_lat = min_lng = 181
+    max_lat = max_lng = -181
+    for pt in pt_list:
+            # geojson store lng first, then lat...
+        lat = pt[1]
+        lng = pt[0]
+        if lat > max_lat:
+            max_lat = lat
+        if lat < min_lat:
+            min_lat = lat
+        if lng > max_lng:
+            max_lng = lng
+        if lng < min_lng:
+            min_lng = lng
+    return min_lat, max_lat, min_lng, max_lng
+
+
+def get_bboxes(pt_list, return_lat_first=True):
     '''There are some cases that across the prime merdian (and +-180 deg). To solve that problem,
     one workaround solution is to return a list of bboxes'''
 
-    min_lat = min_lng = 181
-    max_lat = max_lng = -181
-
-    def find_xtrem_coord(min_lat, max_lat, min_lng, max_lng):
-        '''Find the min&max lat&lng for the feature'''
-        for pt in pt_list:
-            # geojson store lng first, then lat...
-            lat = pt[1]
-            lng = pt[0]
-            if lat > max_lat:
-                max_lat = lat
-            if lat < min_lat:
-                min_lat = lat
-            if lng > max_lng:
-                max_lng = lng
-            if lng < min_lng:
-                min_lng = lng
-
-    def format_output(lat_ranges):
+    def format_output(lng_ranges):
         '''Based on desired lat, lng order, return the list of bbox'''
         bboxes = []
-        for lat_range in lat_ranges:
+        for lng_range in lng_ranges:
             if return_lat_first:
-                top_left = tuple(lat_range[0], max_lng)
-                bottom_right = tuple(lat_range[1], min_lng)
-                bboxes.insert(tuple(top_left, bottom_right))
+                top_left = (max_lat, lng_range[0])
+                bottom_right = (min_lat, lng_range[1])
+                bboxes.append((top_left, bottom_right))
             else:
-                top_left = tuple(max_lng, lat_range[0])
-                bottom_right = tuple(min_lng, lat_range[1])
-                bboxes.insert(tuple(top_left, bottom_right))
+                top_left = (lng_range[0], max_lat)
+                bottom_right = (lng_range[1], min_lat)
+                bboxes.append((top_left, bottom_right))
         return bboxes
 
-    find_xtrem_coord(min_lat, max_lat, min_lng, max_lng)
+    min_lat, max_lat, min_lng, max_lng = find_xtrem_coord(pt_list)
 
-    # Because there is no warp-around issue for lng, we only need to
-    # differentiate lat
-    if min_lat < 0 and max_lat > 0:
+    # Because there is no warp-around issue for lat, we only need to
+    # differentiate lng
+    if min_lng < 0 and max_lng > 0:
         # find the smallest distance of two different cases first.
-        abs_dist_pass_0 = max_lat - min_lat
-        abs_dist_pass_180 = 2 * 180 - max_lat + min_lat
+        abs_dist_pass_0 = max_lng - min_lng
+        abs_dist_pass_180 = 2 * 180 - max_lng + min_lng
         if abs_dist_pass_0 < abs_dist_pass_180:
             # try if the smallest distance is correct. It should be the case for most
             # of the time, because normally no feature would occupy more than 180 deg
@@ -75,21 +79,63 @@ def get_bboxes(pt_list, return_lat_first):
             # Check to see if the coordinates of the feature fall into the range. If
             # not, then we simply choose the wrong one, need to get an opposite
             # range
-            lat_range = [tuple(min_lat, max_lat)]
-            if is_correct_lat_range(lat_range, pt_list):
-                return format_output(lat_range)
+            lng_range = [(min_lng, max_lng)]
+            if is_correct_lng_range(lng_range, pt_list):
+                return format_output(lng_range)
             else:
-                lat_range = [tuple(max_lat, 180 - 1e13), tuple(-180 + 1e13, min_lng)]
-                return format_output(lat_range)
+                print 'A MISS!'
+                lng_range = [(max_lng, 180 - 1e13),
+                             (-180 + 1e13, min_lng)]
+                return format_output(lng_range)
         else:
-            lat_range = [tuple(max_lat, 180 - 1e13),
-                         tuple(-180 + 1e13, min_lng)]
-            if is_correct_lat_range(lat_range, pt_list):
-                return format_output(lat_range)
+            lng_range = [(max_lng, 180 - 1e13),
+                         (-180 + 1e13, min_lng)]
+            if is_correct_lng_range(lng_range, pt_list):
+                return format_output(lng_range)
             else:
-                lat_range = [tuple(min_lat, max_lat)]
-                return format_output(lat_range)
+                print 'A MISS!'
+                lng_range = [(min_lng, max_lng)]
+                return format_output(lng_range)
     else:
-        lat_range = [tuple(min_lat, max_lat)]
-        return format_output(lat_range)
+        lng_range = [(min_lng, max_lng)]
+        return format_output(lng_range)
+    assert False, "Should never reach here"
+    return []
 
+
+def get_pt_list(feature):
+    '''Get the point list of the single feature. Regardless of it is "multi-" feature or not,
+    return the data in the same format:
+
+    list[list(lng, lat)], the order is specified by GeoJson'''
+
+    feature_type = feature['geometry']['type']
+    feature_coord = feature['geometry']['coordinates']
+
+    def case_point():
+        return [feature_coord]
+
+    def case_linestr_mulpt():
+        return feature_coord
+
+    def case_polygon_mullinestr():
+        result = []
+        for coords in feature_coord:
+            result.extend(coords)
+        return result
+
+    def case_mulpolygon():
+        result = []
+        for polygon in feature_coord:
+            for coords in polygon:
+                result.extend(coords)
+        return result
+
+    if feature_type == "Point":
+        return case_point()
+    if feature_type == "LineString" or feature_type == "MultiPoint":
+        return case_linestr_mulpt()
+    if feature_type == "Polygon" or feature_type == "MultiLineString":
+        return case_polygon_mullinestr()
+    if feature_type == "MultiPolygon":
+        return case_mulpolygon()
