c0357c145ee6052d98686d074cb1b47b6fd3789d
Ryan Hunt <ryhunt@mymail.mines.edu>
2017-06-06 03:53:46 -0600

    Partially finished.

--- JSL/src/main/java/edu/mines/csci370/GeoHandler.java
+++ JSL/src/main/java/edu/mines/csci370/GeoHandler.java
@@ -19,6 +19,7 @@ import com.google.common.geometry.S2LatLngRect;
 import com.google.common.geometry.S2RegionCoverer;
 
 import org.json.simple.JSONObject;
+import org.json.simple.JSONArray;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 
@@ -50,40 +51,39 @@ public class GeoHandler implements GeolocationService.Iface {
 
     // Lookup the Cells in the Database
     List<Feature> results = new ArrayList<>();
-    Map<String, Map<Long, JSONObject>> pieces = new HashMap<>();
-    Map<String, Map<Long, Long>> changeDate = new HashMap<>();
-    Map<String, Long> timestamps = new HashMap<>();
+    Map<String /* OSM_ID */, Map<Long /* S2 Cell ID */, JSONObject /* GeoJSON */>> pieces = new HashMap<>();
+    Map<String /* OSM_ID */, Map<Long /* S2 Cell ID */, Long /* TIMESTAMP */>> changeDate = new HashMap<>();
+    Map<String /* OSM_ID */, Long /* TIMESTAMP */> timestamps = new HashMap<>();
 
     Session session = Database.getSession();
     PreparedStatement statement = Database.prepareFromCache(
       "SELECT unixTimestampOf(time) AS time_unix, json, osm_id FROM global.slave WHERE level=? AND s2_id=? AND time >= ?");
-
-    // Historical Query info
-    // System.out.println(timestampMillis);
-    // mintimeuuid: YYYY-MM-DD hh:mm+____
 
     // Execute the Query
     for (S2CellId cell : cells) {
       ResultSet rs = session.execute(statement.bind(level, cell.id(), UUIDs.startOf(timestampMillis)));
 
       while (!rs.isExhausted()) {
+        // Parse the row from the Database
         Row row = rs.one();
         String json = row.getString("json");
         String osmId = row.getString("osm_id");
         Long timestamp = row.getLong("time_unix");
 
         try {
+          // Handle Initial Conditions
           if (!pieces.containsKey(osmId))
             pieces.put(osmId, new HashMap<Long, JSONObject>());
           if (!changeDate.containsKey(osmId))
             changeDate.put(osmId, new HashMap<Long, Long>());
 
+          // If this is the Oldest Revision, Take It
           if (!changeDate.get(osmId).containsKey(cell.id())
             || changeDate.get(osmId).get(cell.id()) > timestamp) {
             pieces.get(osmId).put(cell.id(), (JSONObject) (new JSONParser()).parse(json));
             timestamps.put(osmId, timestamp);
 
-            results.add(new Feature(timestamp, json));
+           // results.add(new Feature(timestamp, json));
           }
           
         } catch (ParseException e) {
@@ -93,15 +93,17 @@ public class GeoHandler implements GeolocationService.Iface {
     }
 
     // Recombine Features
-    /*for (String osmId : pieces.keySet()) {
+    for (String osmId : pieces.keySet()) {
       Map<Long, JSONObject> list = pieces.get(osmId);
 
       JSONObject unified = reconstruct(list);
-      results.add(new Feature(
-        timestamps.get(osmId),
-        unified.toString()
-      ));
-    }*/
+      if (unified != null) {
+        results.add(new Feature(
+          timestamps.get(osmId),
+          unified.toString()
+        ));
+      }
+    }
 
     long finish = System.currentTimeMillis();
     System.out.println(cells.size() + " queries @ scale=" + level + " in " + (finish - start) + "ms");
@@ -123,10 +125,6 @@ public class GeoHandler implements GeolocationService.Iface {
     PreparedStatement statement = Database.prepareFromCache(
       "SELECT unixTimestampOf(time) AS time_unix, json FROM global.slave WHERE level=? AND s2_id=? AND time >= ?");
 
-    // Historical Query info
-    // System.out.println(timestampMillis);
-    // mintimeuuid: YYYY-MM-DD hh:mm+____
-
     // Execute the Query
     ResultSet rs = session.execute(statement.bind(cell.level(), cell.id(), UUIDs.startOf(timestampMillis)));
 
@@ -143,7 +141,157 @@ public class GeoHandler implements GeolocationService.Iface {
     return results;
   }
 
-  private JSONObject reconstruct(Map<Long, JSONObject> map) {
+  private JSONObject reconstruct(Map<Long, JSONObject> s2CellToJson) {
+    // Simple Case == Already Reconstructed
+    if (s2CellToJson.size() == 1)
+      for (Long key : s2CellToJson.keySet())
+        return s2CellToJson.get(key);
+
+    // Switch on Feature Type
+    Long key = s2CellToJson.keySet().iterator().next();
+    String type = (String) ((JSONObject) ((JSONObject) s2CellToJson.get(key)).get("geometry")).get("type");
+
+    if (type.equals("Point") || type.equals("MultiPoint"))
+      return reconstructMultiPoint(s2CellToJson);
+    else if (type.equals("LineString") || type.equals("MultiLineString"))
+      return reconstructMultiLineString(s2CellToJson);
+    else if (type.equals("Polygon") || type.equals("MultiPolygon"))
+      return reconstructMultiPolygon(s2CellToJson);
+    else return null;
+  }
+
+  private JSONObject reconstructMultiPoint(Map<Long, JSONObject> s2CellToJson) {
+    // Iterate Cells and Combine Coordinates
+    JSONArray coordinates = new JSONArray();
+    for (Long cellID : s2CellToJson.keySet()) {
+      String type = (String) ((JSONObject) ((JSONObject) s2CellToJson.get(cellID)).get("geometry")).get("type");
+
+      if (type.equals("Point"))
+        coordinates.add((JSONArray) ((JSONObject) ((JSONObject) s2CellToJson.get(cellID)).get("geometry")).get("coordinates"));
+      else
+        coordinates.addAll((JSONArray) ((JSONObject) ((JSONObject) s2CellToJson.get(cellID)).get("geometry")).get("coordinates"));
+    }
+
+    // Build a new Points JSON
+    Long key = s2CellToJson.keySet().iterator().next();
+    ((JSONObject) ((JSONObject) s2CellToJson.get(key)).get("geometry")).put("coordinates", coordinates.size() > 1 ? coordinates : coordinates.get(0));
+    ((JSONObject) ((JSONObject) s2CellToJson.get(key)).get("geometry")).put("type", coordinates.size() > 1 ? "MultiPoint" : "Point");
+    System.out.println(s2CellToJson.size() + " down to 1");
+    return s2CellToJson.get(key);
+  }
+
+  private JSONObject reconstructMultiLineString(Map<Long, JSONObject> s2CellToJson) {
+    // Naive Approach -- Throw Away "Fake" Points
+    List<JSONArray> results = new ArrayList<>();
+
+    // Put all Line Segments into Map
+    Map<Long, List<JSONArray>> lines = new HashMap<>();
+    Map<Long, List<JSONArray>> processed = new HashMap<>();
+    for (Long cellID : s2CellToJson.keySet()) {
+      String type = (String) ((JSONObject) ((JSONObject) s2CellToJson.get(cellID)).get("geometry")).get("type");
+
+      // Add list of partial "lines" to the Map
+      JSONArray array = new JSONArray();
+      if (type.equals("LineString"))
+        array.add((JSONArray) ((JSONObject) ((JSONObject) s2CellToJson.get(cellID)).get("geometry")).get("coordinates"));
+      else
+        array.addAll((JSONArray) ((JSONObject) ((JSONObject) s2CellToJson.get(cellID)).get("geometry")).get("coordinates"));
+      lines.put(cellID, array);
+    }
+
+    Long seed = null;
+    JSONArray lineSegment = null, lineComplete = null;
+
+    // Loop Until Done
+    while (lines.size() > 0) {
+
+      // Choose a Seed to Start With
+      if (lineSegment == null) {
+        seed = lines.keySet().iterator().next();
+        lineSegment = lines.get(seed).get(0);
+        if (!processed.containsKey(seed)) processed.put(seed, new JSONArray());
+        processed.get(seed).add(lineSegment); lines.get(seed).remove(0);
+        if (lines.get(seed).size() == 0) lines.remove(seed);
+        lineComplete = new JSONArray(); results.add(lineComplete);
+      }
+
+      // Traverse Line Segment
+      for (int i = 0; i < lineSegment.size(); i++) {
+        lineComplete.add(lineSegment.get(i));
+
+        // Replace Scalia
+        if (i == lineSegment.size() - 1 && ((JSONArray) lineSegment.get(i)).size() == 3) {
+          String code = (String) ((JSONArray) lineSegment.get(i)).get(2);
+          char direction = code.charAt(2);
+          assert(code.startsWith("EL"));
+
+          // Make Decision for Next Cell
+          S2CellId[] neighbors = new S2CellId[4];
+          (new S2CellId(seed)).getEdgeNeighbors(neighbors);
+          Long nextCell = (direction == 'D' ? neighbors[0].id() : (direction == 'R' ? neighbors[1].id() : (direction == 'U' ? neighbors[2].id() : neighbors[3].id())));
+
+          // Check if Continuation is Unsearched
+          if (lines.containsKey(nextCell)) {
+            for (JSONArray tmpSegment : lines.get(nextCell)) {
+              if ((double) ((JSONArray) tmpSegment.get(0)).get(0) == (double) ((JSONArray) lineSegment.get(i)).get(0)
+                    && (double) ((JSONArray) tmpSegment.get(0)).get(1) == (double) ((JSONArray) lineSegment.get(i)).get(1))
+                seed = nextCell; lineSegment = tmpSegment;
+            }
+            if (seed == nextCell) break;
+          }
+
+          // Check if Continuation is Searched
+          if (processed.containsKey(nextCell)) {
+            for (JSONArray tmpSegment : processed.get(nextCell)) {
+              if ((double) ((JSONArray) tmpSegment.get(0)).get(0) == (double) ((JSONArray) lineSegment.get(i)).get(0)
+                    && (double) ((JSONArray) tmpSegment.get(0)).get(1) == (double) ((JSONArray) lineSegment.get(i)).get(1)) {
+
+                // Search for Existing lineComplete
+                for (JSONArray tmpLineComplete : results) {
+                  if ((double) ((JSONArray) tmpLineComplete.get(0)).get(0) == (double) ((JSONArray) lineSegment.get(i)).get(0)
+                      && (double) ((JSONArray) tmpLineComplete.get(0)).get(1) == (double) ((JSONArray) lineSegment.get(i)).get(1)) {
+                    tmpLineComplete.addAll(0, lineSegment);
+                    seed = null; lineSegment = null;
+                    break;
+                  }
+                }
+                if (seed == null) break;
+              }
+            }
+          }
+
+          // We Fell off the Map (or already merged)
+          seed = null; lineSegment = null;
+        }
+      }
+    }
+
+    // Trim "Fake" News
+    for (JSONArray lineCompleted : results) {
+      for (int i = 1; i < lineCompleted.size() - 1; i++) {
+        JSONArray point = (JSONArray) lineCompleted.get(i);
+        if (point.size() > 2)
+          point = (JSONArray) point.subList(0, 2);
+      }
+    }
+
+    // Return the Results
+    Long finishedSeed = s2CellToJson.keySet().iterator().next();
+    ArrayList coordinates = new ArrayList();
+    for (ArrayList finishedLineSegment : results)
+      coordinates.add(finishedLineSegment);
+    if (coordinates.size() == 1) coordinates = (JSONArray) coordinates.get(0);
+    ((JSONObject) ((JSONObject) s2CellToJson.get(finishedSeed)).get("geometry")).put("coordinates", coordinates);
+    ((JSONObject) ((JSONObject) s2CellToJson.get(finishedSeed)).get("geometry")).put("type", coordinates.size() > 1 ? "MultiLineString" : "LineString");
+    return s2CellToJson.get(finishedSeed);
+  }
+
+  private JSONObject reconstructMultiPolygon(Map<Long, JSONObject> s2CellToJson) {
+    // Condense MultiPolygon->MultiLineString, Polygon->LineString
+    // Call reconstructMutliLine()
+    // Process output by closing each polygon and wrapping corners Clockwise
+    // Return Polygon/MultiPolygon object
+
     return null;
   }
 }
