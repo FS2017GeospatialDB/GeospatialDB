c5841e96374c063a6d7bc7d369ef6fd73d98f4c4
Ryan Hunt <ryhunt@mymail.mines.edu>
2017-06-15 13:43:53 -0600

    Modernizing...

--- ryan_schema_design/load.py
+++ ryan_schema_design/load.py
@@ -1,44 +1,129 @@
 import sys
+import time
+import json
+import ijson
+import slice
 import ctypes
-import geojson
+import logging
+import cassandra
+
 from cassandra.cluster import Cluster
+
+# Setup Logging
+logging.basicConfig(filename="/var/tmp/load_features.log", filemode="w", level=logging.INFO)
 
 # Print Usage Instructions
 def printUsageAndExit(statusCode):
-    print "Usage: ", sys.argv[0], " [spatialData.json]"
+    print "Usage: ", sys.argv[0], " [spatialData1.json ... spatialDataN.json]"
     print
     sys.exit(statusCode)
 
 # Fill the Database
-def load():
+def load(fileList):
+    """
+    Load a GeoJSON file/files (specified by fileList) into the Cassandra database.
 
-    # Read the JSON
-    data = None
-    with open(sys.argv[1], 'r') as inFile:
-        data = geojson.loads(inFile.read())
+    Dialect: GeoJSON converted from OSM by ogr2ogr
+    """
 
     # Connect to the Database
-    cluster = Cluster()
-    session = cluster.connect('global')
+    logging.info("Connecting to Cluster...")
+    session = None
+    hosts = ['127.0.0.1', '138.67.187.226', '138.67.187.225', '138.67.187.149', '138.67.187.228', '138.67.186.219',
+           '138.67.187.218', '138.67.187.137', '138.67.186.191', '138.67.187.223', '138.67.186.221', '138.67.187.205']
+    for host in hosts:
+        try:
+            cluster = Cluster([host])
+            session = cluster.connect('global')
+            break
+        except:
+            logging.info("Failed to connect to host %s...", host)
+    
+    if session is None:
+        logging.info("Failed to connect to any host in %s. Exiting...", hosts)
+        return -2
 
-    # Prepare a Statement
+    # Prepared Statements
+    master_select_ps = session.prepare('''
+        SELECT * FROM master WHERE osm_id=?
+    ''')
     master_insert_ps = session.prepare('''
         INSERT INTO master (osm_id, json) VALUES (?, ?)
     ''')
+    slave_insert_ps = session.prepare('''
+        INSERT INTO slave(level, s2_id, time, osm_id, is_cut, json)
+        VALUES (?, ?, ?, ?, ?, ?)
+    ''')
 
-    # Perform Insertions (or Updates)
-    for feature in data['features']:
-        osm_id = feature['id']
-        json = geojson.dumps(feature)
-        session.execute(master_insert_ps, (osm_id, json))
+    # Read the JSONs
+    logging.info("Received JSON list: %s", fileList)
+    for filename in fileList:
+        logging.info("Opening JSON (%s)...", filename)
+        with open(filename, 'r') as inFile:
+            for feature in jsonItems(inFile, 'features.item'):
+                start = time.time()
+
+                # Extract ID
+                osm_id = feature['id']
+
+                # Fix Timeout Issue
+                success = False
+                while not success:
+                    try:
+                        # Check if ID already exists
+                        if session.execute(master_select_ps, (osm_id,)):
+                            logging.info("ID %s already exists in database. Skipping...", osm_id)
+                            break
+
+                        # Insert Feature into Master
+                        logging.info("Inserting %s into Master...", osm_id)
+                        myjson = json.dumps(feature)
+                        session.execute(master_insert_ps, (osm_id, myjson))
+
+                        # Insert Feature into Slave
+                        logging.info("Inserting %s into Slave...", osm_id)
+                        for level in [4, 8, 12]:
+                            pieces = slice.slice_feature(feature, level)
+                            for cellID,cellJson in pieces.items():
+                                cellID = (cellID + 2**63) % 2**64 - 2**63                       # Convert to 64-bit Signed Integer
+                                cellJson = json.dumps(cellJson)
+                                
+                                session.execute(slave_insert_ps, (level, cellID, cassandra.util.uuid_from_time(int(time.time()), 0, 0), osm_id, True))
+                                session.execute(slave_insert_ps, (level, cellID, cassandra.util.HIGHEST_TIME_UUID, osm_id, True, cellJson))
+                        success = True
+                    except:
+                        pass
+
+                # Done
+                end = time.time()
+                logging.info("Finished with %s in %f seconds...", osm_id, (end - start))
+        logging.info("Closing %s...", filename)
 
     # Cleanup
+    logging.info("Done! %d files processed...", len(fileList))
     cluster.shutdown()
+
+def jsonItems(file, prefix):
+    items = ijson.parse(file)
+    try:
+        while True:
+            current, event, value = next(items)
+            if current == prefix:
+                builder = ijson.common.ObjectBuilder()
+                end_event = event.replace('start', 'end')
+                while (current, event) != (prefix, end_event):
+                    if event == 'number':
+                        value = float(value)
+                    builder.event(event, value)
+                    current, event, value = next(items)
+                yield builder.value
+    except StopIteration:
+        pass
 
 if __name__ == '__main__':
 
     # Check Command-Line Arguments
-    if len(sys.argv) != 2:
+    if len(sys.argv) < 2:
         printUsageAndExit(-1)
 
-    load()
+    load(sys.argv[1:])
